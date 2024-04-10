fb1853e5424189e84260139b0ea6023bc048edd5
Jhojany Uzcategui <externo.apside102@consorcio.cl>
2024-04-10 14:27:00 -0400

    Fix server.js

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const path = require('path');
-const serveStatic = require('serve-static');
+import express from 'express';
+import path from 'path';
+import serveStatic from 'serve-static';
 
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist'), {'index': ['index.html', 'index.htm']}));
+const app = express();
+app.use(serveStatic(path.join(path.resolve(), 'dist'), {'index': ['index.html', 'index.htm']}));
 
 const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log('Servidor iniciado en el puerto: ' + port);
+
